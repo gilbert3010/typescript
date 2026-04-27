b6180ebbb6abb813a60427d2909412d4f7274c1c
gilbert3010 <gilbert.meneses30@gmail.com>
2026-04-27 17:37:43 -0400

    practica-javascript

--- strings/js/metodos.js
+++ strings/js/metodos.js
@@ -1,7 +1,8 @@
-const mensaje = "Mensaje de texto, 'comprobando comillas'"
-const complemento = "conectando caracteres"
 
-console.log(mensaje)
+/*const mensaje = "Mensaje de texto, 'comprobando comillas'"
+const complemento = "conectando caracteres"*/
+
+//console.log(mensaje)
 
 //console.log(mensaje.length) // legth es una propiedad para medir la cantidad de caracteres en una cadenas
 
@@ -9,4 +10,43 @@ console.log(mensaje)
 
 //console.log(mensaje + complemento) // otra forma de conectar cadenas de texto
 
-console.log(`${mensaje} ${complemento}`) // otra forma de conectar cadenas de texto usando template literals (backticks) y expresiones ${} para insertar variables o resultados de operaciones dentro de la cadena.
+//console.log(`${mensaje} ${complemento}`) // otra forma de conectar cadenas de texto usando template literals (backticks) y expresiones ${} para insertar variables o resultados de operaciones dentro de la cadena.
+
+/*const arreglo1 = [1, 2, 3,4,5,6,7,8,9,10];
+
+console.log(arreglo1);
+
+const arreglo2 = new Array(1,2,3,4,5,6);
+console.log(arreglo2);*/
+
+/*const Persona = {
+    nombre: "Gilberto",
+    apellido: "Meneses",
+    edad: 22,
+    Usuario: {
+        username: "GILBERT",
+        correo: "gilbert0123@gmail.com",
+        password: "1234hdhd"
+    },
+    pais: "Venezuela"
+}
+
+Object.freeze(Persona)
+
+
+console.table(Persona)
+console.log(Object.isFrozen(Persona))*/
+
+"user strict";
+
+const Estudiante={
+    nombre: "Gilberto",
+    apellido: "Meneses",
+    grado: "5to año"
+}
+
+Object.seal(Estudiante)
+Estudiante.nombre = "Gerardo"
+
+console.table(Estudiante)
+console.log(Object.isSealed(Estudiante))
